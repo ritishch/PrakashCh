2a24a3729142d93ed2df36a52b6fdcd1f96061b1
Prakash <cjprakash522@gmail.com>
2024-04-25 17:19:10 +0530

    first commit

---
+++ src/test/java/com/sathya/web/SpringBootWebUser1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sathya.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebUser1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
